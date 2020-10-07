aa645a777111297e03f2148c2b708bc25b426ad4
Sergio Perea de la Casa <spc00033@red.ujaen.es>
2020-10-08 00:00:57 +0200

    Evitar disminuir menos de 1 caja en pantalla.

--- pr02_IGV/pr02_IGV/igvInterfaz.cpp
+++ pr02_IGV/pr02_IGV/igvInterfaz.cpp
@@ -63,19 +63,22 @@ void igvInterfaz::set_glutKeyboardFunc(unsigned char key, int x, int y) {
 		interfaz.escena.incrementar_tamX(1);
 		break;
 	case 'X': // añade una replica de el/los objeto/s "Caja de zapatos" a partir del desplazamiento en el eje x en +1.
-		interfaz.escena.decrementar_tamX(1);
+		if(interfaz.escena.get_tamX() > 0)
+			interfaz.escena.decrementar_tamX(1);
 		break;
 	case 'y': // añade una replica de el/los objeto/s "Caja de zapatos" a partir del desplazamiento en el eje y en +1.
 		interfaz.escena.incrementar_tamY(1);
 		break;
 	case 'Y': // añade una replica de el/los objeto/s "Caja de zapatos" a partir del desplazamiento en el eje y en +1.
-		interfaz.escena.decrementar_tamY(1);
+		if (interfaz.escena.get_tamY() > 0)
+			interfaz.escena.decrementar_tamY(1);
 		break;
 	case 'z': // añade una replica de el/los objeto/s "Caja de zapatos" a partir del desplazamiento en el eje z en +1.
 		interfaz.escena.incrementar_tamZ(1);
 		break;
 	case 'Z': // añade una replica de el/los objeto/s "Caja de zapatos" a partir del desplazamiento en el eje z en +1.
-		interfaz.escena.decrementar_tamZ(1);
+		if (interfaz.escena.get_tamZ() > 0)
+			interfaz.escena.decrementar_tamZ(1);
 		break;
 	case 27: // tecla de escape para SALIR
 		exit(1);
