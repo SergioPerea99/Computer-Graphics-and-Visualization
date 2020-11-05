a3d117779446212983643862d3b54c920714fcfe
Sergio Perea de la Casa <spc00033@red.ujaen.es>
2020-11-05 20:11:28 +0100

    Pr3a: Apartado B hecho.
    Creado para la creación de cualquier forma a partir de ángulos con el número de divisiones de caras (2 triangulos) y divisiones en altura.

--- Pr03_a_IGV/igvCilindro.cpp
+++ Pr03_a_IGV/igvCilindro.cpp
@@ -20,26 +20,27 @@ igvCilindro::igvCilindro(float r, float a, int divU, int divV)
 
 	/*Instancia de los indices de arriba a abajo en sentido antihorario.*/
 	
-	num_triangulos = (divU*2) * divV; //Formula sacada de hacer comprobaciones.
+	num_triangulos = (divU*2) * divV + 2; //Formula sacada de hacer comprobaciones. El +2 es para contar la union de 1 extremo con el otro, al final de la iteración.
 	triangulos = new unsigned [num_triangulos * 3];
 	
 	int cont_triangulos = 0; k = 0; /*Con la i voy a controlar el numero de triangulos añadidos.*/
 	
-	/*Triangulo que empieza por el vértice superior izquierdo (relativo a un rectangulo que conecta 2 triangulos).*/
-	for (int j = 0; j+1 <= divU; j++) {
-		triangulos[k++] = divU + j + 1; /*Vertice superior izquierdo*/
-		triangulos[k++] = j; /*Vertice inferior izquierdo*/
-		triangulos[k++] = (divU+1) + j + 1; /*Vertice superior siguiente.*/
-	}
+		/*Triangulo que empieza por el vértice superior izquierdo (relativo a un rectangulo que conecta 2 triangulos).*/
+		for (int j = 0; j <= (int)num_triangulos/2 - 1; j++) {
+			triangulos[k++] = divU + j + 1; /*Vertice superior izquierdo*/
+			triangulos[k++] = j; /*Vertice inferior izquierdo*/
+			triangulos[k++] = (divU + 1) + j + 1; /*Vertice superior siguiente.*/
+			cont_triangulos++;
+		}
 
-	/*Triangulo que empieza por el vértice superior derecho (relativo a un rectangulo que conecta 2 triangulos).*/
-	for (int j = 0; j + 1 <= divU; j++) {
-		triangulos[k++] = divU + 1 + j + 1; /*Vertice superior derecho*/
-		triangulos[k++] = j; /*Vertice inferior izquierdo*/
-		triangulos[k++] = j + 1; /*Vertice inferior siguiente.*/
-	}
-	
-	
+		/*Triangulo que empieza por el vértice superior derecho (relativo a un rectangulo que conecta 2 triangulos).*/
+		for (int j = 0; j <= (int)num_triangulos / 2 - 1; j++) {
+			triangulos[k++] = divU + 1 + j + 1; /*Vertice superior derecho*/
+			triangulos[k++] = j; /*Vertice inferior izquierdo*/
+			triangulos[k++] = j + 1; /*Vertice inferior siguiente.*/
+			cont_triangulos++;
+		}
+		
 	/* Apartado C: Añadir el vector de normales */
 }
 
@@ -50,7 +51,7 @@ igvCilindro::~igvCilindro()
 
 int igvCilindro::creaCircunferencia(float _altura,float r, int divU, int k) {
 	float radianes = 0; float aux;
-	while (radianes <= 2*PI*r) {
+	while (radianes < 2*PI*r) { /*Lo dejo en < estricto ya que debe de ser por igual el calculo del radian con valor 0 que con valor 2PI*R */
 		vertices[k++] = cos(radianes) * r; aux = vertices[k - 1];
 		vertices[k++] = _altura;  aux = vertices[k - 1];
 		vertices[k++] = sin(radianes) * r;  aux = vertices[k - 1];
